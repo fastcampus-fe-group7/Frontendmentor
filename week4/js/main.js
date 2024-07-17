0e9a79150a954647f72f89cc3cd48e92e5846061
LeeYunHwan <cyzhqly@gmail.com>
2024-07-17 19:49:58 +0900

    week5 연산창 구현 및 코드 수정

--- week4/js/main.js
+++ week4/js/main.js
@@ -1,7 +1,8 @@
 //? 결과 스크린
 const resultScreen=document.querySelector('.calculator-result');
+const opsScreen=document.querySelector('.calculator-operations');
 
-//?numbers
+//? numbers
 const num0Btn=document.querySelector('#num0');
 const num1Btn=document.querySelector('#num1');
 const num2Btn=document.querySelector('#num2');
@@ -45,38 +46,49 @@ let curResult=0;
 //* 계산 초기화버튼
 resetBtn.addEventListener('click',()=>{
   resultScreen.textContent='';
+  opsScreen.textContent='';
 });
 
 //* 숫자 입력
 num0Btn.addEventListener('click',()=>{
   resultScreen.textContent+="0"
+  opsScreen.textContent+="0"
 });
 num1Btn.addEventListener('click',()=>{
   resultScreen.textContent+="1"
+  opsScreen.textContent+="1"
 });
 num2Btn.addEventListener('click',()=>{
   resultScreen.textContent+="2"
+  opsScreen.textContent+="2"
 });
 num3Btn.addEventListener('click',()=>{
   resultScreen.textContent+="3"
+  opsScreen.textContent+="3"
 });
 num4Btn.addEventListener('click',()=>{
   resultScreen.textContent+="4"
+  opsScreen.textContent+="4"
 });
 num5Btn.addEventListener('click',()=>{
   resultScreen.textContent+="5"
+  opsScreen.textContent+="5"
 });
 num6Btn.addEventListener('click',()=>{
   resultScreen.textContent+="6"
+  opsScreen.textContent+="6"
 });
 num7Btn.addEventListener('click',()=>{
   resultScreen.textContent+="7"
+  opsScreen.textContent+="7"
 });
 num8Btn.addEventListener('click',()=>{
   resultScreen.textContent+="8"
+  opsScreen.textContent+="8"
 });
 num9Btn.addEventListener('click',()=>{
   resultScreen.textContent+="9"
+  opsScreen.textContent+="9"
 });
 
 // const buttons=document.querySelectorAll('button[type="button"]');
@@ -98,6 +110,8 @@ plusBtn.addEventListener('click',()=>{
     resultScreen.textContent="";
     isPlusTrue=true;
 
+    opsScreen.textContent+="+";
+
     console.log("status:Plus", curResult);
   }
 });
@@ -108,6 +122,8 @@ minusBtn.addEventListener('click',()=>{
     curResult=resultScreen.textContent;
     resultScreen.textContent="";
     isMinusTrue=true;
+
+    opsScreen.textContent+="-";
 
     console.log("status:Minus", curResult);
   }
@@ -120,6 +136,8 @@ divBtn.addEventListener('click',()=>{
     resultScreen.textContent="";
     isDivTrue=true;
 
+    opsScreen.textContent+="/";
+
     console.log("status:Divide", curResult);
   }
 });
@@ -131,6 +149,8 @@ mulBtn.addEventListener('click',()=>{
     resultScreen.textContent="";
     isMulTrue=true;
 
+    opsScreen.textContent+="*";
+
     console.log("status:Multiply", curResult);
   }
 });
@@ -138,7 +158,9 @@ mulBtn.addEventListener('click',()=>{
 //delete
 delBtn.addEventListener('click',()=>{
   if(resultScreen.textContent.length > 0){
-    resultScreen.textContent=resultScreen.textContent.slice(0,-1);
+    let result=resultScreen.textContent.slice(0,-1);
+    resultScreen.textContent=result;
+    opsScreen.textContent=result;
   }
 });
 
@@ -148,18 +170,22 @@ dotBtn.addEventListener('click',()=>{
     curResult=resultScreen.textContent;
     curResult+=".";
     resultScreen.textContent=curResult;
+
+    opsScreen.textContent+=".";
+
     console.log(curResult);
   }
 });
 
-//reset
+//result
 resultBtn.addEventListener('click',()=>{
-  let finResult=parseFloat(curResult)
-  if(isPlusTrue===true){
+  let finResult=parseFloat(curResult);
+
+  if(isPlusTrue){
     finResult+=parseFloat(resultScreen.textContent);
-  }else if(isMinusTrue===true){
+  }else if(isMinusTrue){
     finResult-=parseFloat(resultScreen.textContent);
-  }else if(isDivTrue===true){
+  }else if(isDivTrue){
     finResult/=parseFloat(resultScreen.textContent);
   }else{
     finResult*=parseFloat(resultScreen.textContent);
@@ -169,8 +195,7 @@ resultBtn.addEventListener('click',()=>{
   isMinusTrue=false;
   isDivTrue=false;
   isMulTrue=false;
-
+  
+  opsScreen.textContent=finResult;
   resultScreen.textContent=finResult;
-  // curResult.textContent='';
-  // console.log(curResult);
 });
